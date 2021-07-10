af4fd7be94a5c5c49a89106ebdb1bb38e8740d90
umeshpatel2001 <70634279+umeshpatel2001@users.noreply.github.com>
2021-07-10 14:55:56 +0530

    change js file

--- javaScript/app.js
+++ javaScript/app.js
@@ -3,6 +3,7 @@ showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     // if (addTxt=="") {
     //     alert("Enter Some Notes !!!!!!");
     //     continue;
@@ -15,9 +16,15 @@ addBtn.addEventListener("click", function (e) {
         notesObj = JSON.parse(notes);
 
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title :addTitle.value,
+        text :addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = " ";
+    addTitle.value = " ";
+
     showNotes();
 })
 
@@ -34,8 +41,8 @@ function showNotes() {
         html = html + `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
-                    <p class="card-text">${element}</p>
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
                     <button id=${index} onclick="deleteNode(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
             </div>
